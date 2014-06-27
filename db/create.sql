e40ad185dfdcffa7aadb80c8e405569ed1c1ce59
Andre Randel <andre.randel@sectornord.de>
2014-06-27 14:46:46 +0200

    update db/create

--- db/create.sql
+++ db/create.sql
@@ -1,65 +1,32 @@
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `PHP_Shop`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_artikel`
---
-
 CREATE TABLE IF NOT EXISTS `tbl_artikel` (
-  `id` int(11) NOT NULL,
-  `titel` varchar(40) NOT NULL,
-  `preis` varchar(10) NOT NULL,
-  `text` text NOT NULL,
-  `name` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `bezeichnung` varchar(30) NOT NULL,
+  `beschreibung` text NOT NULL,
+  `preis` float NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `bezeichnung` (`bezeichnung`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+
+
+CREATE TABLE IF NOT EXISTS `tbl_artikel_bestellungen` (
+  `artikel_id` int(6) NOT NULL,
+  `bestellung_id` int(6) NOT NULL,
+  PRIMARY KEY (`artikel_id`,`bestellung_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `tbl_data`
---
-
-CREATE TABLE IF NOT EXISTS `tbl_data` (
-  `data_ID` int(11) NOT NULL,
-  `sid` varchar(100) NOT NULL,
-  `emil` varchar(40) NOT NULL,
+CREATE TABLE IF NOT EXISTS `tbl_bestellung` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `email` varchar(40) NOT NULL,
   `name` varchar(40) NOT NULL,
-  `zusatz` varchar(500) NOT NULL,
-  `str` varchar(40) NOT NULL,
-  `plz` int(11) NOT NULL,
-  `ort` varchar(40) NOT NULL,
-  KEY `data_ID` (`data_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_vm`
---
-
-CREATE TABLE IF NOT EXISTS `tbl_vm` (
-  `sid` varchar(40) NOT NULL,
-  `vart` varchar(100) NOT NULL,
-  `zm` varchar(100) NOT NULL,
-  `preis` varchar(8) NOT NULL,
-  `produkte` varchar(100) NOT NULL,
-  `datetime` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  `zusatz` varchar(100) NOT NULL,
+  `str` varchar(50) NOT NULL,
+  `ort` varchar(50) NOT NULL,
+  `land` varchar(40) NOT NULL,
+  `plz` int(5) NOT NULL,
+  `versandArt` varchar(40) NOT NULL,
+  `zahlungsMethode` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
